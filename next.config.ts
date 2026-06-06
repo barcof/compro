4fe1b27f584120ca1d2ac8279426f0900f608766
Barcof <alfiandihakim@gmail.com>
2026-06-06 12:06:00 +0700

    Feat: install and config component

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export', // Mengaktifkan mode Static Site Generation (SSG)
+  images: {
+    unoptimized: true, // Diwajibkan untuk export statis jika menggunakan komponen <Image> Next.js
+  },
 };
 
 export default nextConfig;
